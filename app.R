d057bb0078696c41091202a9e3834720aae77d3c
kareemix <55011084+kareemix@users.noreply.github.com>
2025-06-30 13:01:24 -0400

    sort peptide and feature names

--- app.R
+++ app.R
@@ -9,7 +9,6 @@ options(shiny.port = 3030)
 list_peptides <- c()
 list_peptides_list <- c()
 list_mod_obs <- c()
-# feature_list <- c("b3^1", "b4^1", "b5^1", "b6^1", "ms1", "y10^1", "y11^1", "y4^1", "y5^1", "y6^1", "y7^1", "y8^1", "y9^1")
 feature_list <- c()
 
 
@@ -17,9 +16,10 @@ plot_ui <- function(id, file_text) {
     ns <- NS(id)
     div(
         id = ns("plot_ui"),
-        card(
+        div(
+            hr(),
             file_text,
-            plotOutput(ns("chart"))
+            plotOutput(ns("chart")),
         )
     )
 }
@@ -78,7 +78,8 @@ ui <- fluidPage(
             }
             #top_widgets {
             background-color: white;
-            border-style: ridge;
+            border-style: double;
+            border-width: 5px;
             }
             #chart_container {
             padding-top: 275px;
@@ -116,7 +117,7 @@ ui <- fluidPage(
             tags$div(
                 selectizeInput(
                     "feature_select",
-                    label = h5("Features:"),
+                    label = h5("Select Features:"),
                     choices = NULL,
                     multiple = TRUE
                 ),
@@ -125,8 +126,8 @@ ui <- fluidPage(
         column(
             3,
             tags$div(
-                sliderInput("yfilter", label = h5("Filter by Value"), min = 0, max = 100, value = c(0, 100)),
-                sliderInput("xfilter", label = h5("Filter by Retention Time"), min = 0, max = 100, value = c(0, 100)),
+                sliderInput("yfilter", label = h5("Filter by Value:"), min = 0, max = 100, value = c(0, 100)),
+                sliderInput("xfilter", label = h5("Filter by Retention Time:"), min = 0, max = 100, value = c(0, 100)),
             ),
         ),
     ),
@@ -156,7 +157,7 @@ server <- function(input, output, session) {
         list_peptides <<- c()
         list_peptides_list <<- c()
         file_list <- c()
-        withProgress(message = "Processing Parquet(s)...", {
+        withProgress(message = "Processing Parquet(s)", {
             for (i in seq_along(input$file_name)) {
                 parq <- input$file_name[[i]]
                 dframe_file <- get_df(parq)
@@ -165,7 +166,7 @@ server <- function(input, output, session) {
             }
         })
         names_peptides <- c()
-        withProgress(message = "Processing Peptides...", {
+        withProgress(message = "Processing Peptides", detail = "This may take a while...", {
             for (i in seq_along(file_list)) {
                 dframe_file <- file_list[[i]]
                 list_peptides <<- split.data.frame(dframe_file, dframe_file$pr)
@@ -175,11 +176,13 @@ server <- function(input, output, session) {
                 incProgress(1 / length(file_list))
             }
         })
-        withProgress(message = "Updating Peptide Selection...", {
+        names_peptides <- sort(names_peptides)
+        feature_list <- sort(feature_list)
+        withProgress(message = "Updating Peptide Selection", {
             updateSelectizeInput(session, "peptide_name", choices = names_peptides, server = TRUE)
             updateSelectizeInput(session, "feature_select", choices = feature_list, server = TRUE, selected = feature_list)
         })
-        withProgress(message = "Creating Plot(s)...", {
+        withProgress(message = "Creating Plot(s)", {
             for (i in seq_along(file_list)) {
                 local({
                     this_i <- i
